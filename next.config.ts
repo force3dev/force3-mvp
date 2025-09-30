7fa90f166dda7aa9a5ec17bc7553edd9efe4f60c
Geric <FORCE3APP@GMAIL.COM>
2025-09-30 18:18:23 -0400

    disable ESLint blocking during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    // Don’t fail the build if ESLint finds errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
+/* config options here */
